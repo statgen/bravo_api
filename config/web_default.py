dd9f0cfd6244067fd5c3b9c12fad1c025966e927
Daniel Taliun <dtaliun@browser.sph.umich.edu>
2019-08-16 13:07:16 -0400

    add downloads size

--- config/web_default.py
+++ config/web_default.py
@@ -11,7 +11,8 @@ SUBTITLE = ''
 DATASET_SHORT_NAME = ''
 ARCHIVES = []
 
+# Fill these variables if you want to provide downloads
 DOWNLOADS = False
-DOWNLOAD_CHROMOSOMES_VCF = {}
+DOWNLOAD_CHROMOSOMES_VCF = {} # e.g. { '21': ['100 MB', '/data/vcfs/chr21.vcf.gz'], '22': ['100 MB', '/data/vcfs/chr22.vcf.gz'] }
 
 GOOGLE_ANALYTICS_TRACKING_ID = ''
