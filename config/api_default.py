47af7593236f9f1a60a39ef7ad7a4e63c9636586
Daniel Taliun <dtaliun@browser.sph.umich.edu>
2019-09-26 09:52:26 -0400

    updated default config

--- config/api_default.py
+++ config/api_default.py
@@ -1,4 +1,6 @@
 MONGO_URI = 'mongodb://' # mongodb://<host>:<port>/<database>
 BRAVO_API_PAGE_LIMIT = 10000
 COVERAGE_DIR = '' # directory with coverage files
+SEQUENCES_DIR = '' # directory with BAM/CRAM sequence files
+SEQUENCES_CACHE_DIR = '' # directory for the cached BAM/CRAM files
 GZIP_COMPRESSION = True
