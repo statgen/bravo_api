91cf390d1a08da5f858ab54d6f176f7ca2447c1e
Colin Gross <grosscol@gmail.com>
2021-05-27 14:13:29 -0400

    Clarify sequences dir definition.

--- config/api_default.py
+++ config/api_default.py
@@ -4,17 +4,18 @@ MONGO_URI = 'mongodb://localhost:27017/bravo-demo'  # mongodb://<host>:<port>/<d
 BRAVO_API_PAGE_LIMIT = 10000
 GZIP_COMPRESSION = True
 
-# base directory for data on disk
-BASE_DIR = os.path.join('/', 'data', 'bravo')
+# base directory for data on disk. E.g. /data/bravo
+BASE_DIR = os.path.join(os.sep, 'data', 'bravo')
 
 # directory with coverage files
 COVERAGE_DIR = os.path.join(BASE_DIR, 'coverage')
 
-# directory with BAM/CRAM sequence files
-SEQUENCES_DIR = os.path.join(BASE_DIR, 'crams', 'demo')
+# directory containing variant map and sequences/ dir
+#  Contained sequences dir expected to have with CRAM files
+SEQUENCES_DIR = os.path.join(BASE_DIR, 'crams')
 
 # directory for the runtime cached BAM/CRAM files
 SEQUENCES_CACHE_DIR = os.path.join(BASE_DIR, 'cache')
 
 # path to reference sequence for cram files.  Assumes corresponding .fai exists next to .fa file
-REFERENCE_SEQUENCE = os.path.join(BASE_DIR, 'reference', 'demoChr.fa')
+REFERENCE_SEQUENCE = os.path.join(BASE_DIR, 'reference', 'hg38DH.fa')
