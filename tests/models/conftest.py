52cf764748523896a4d88e8a80dd5a99a017b8c8
Colin Gross <grosscol@gmail.com>
2021-06-30 15:15:51 -0400

    Update coverage dir mock to match expected subdirectory names.

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -5,12 +5,14 @@ from pathlib import Path
 
 
 # Fixture for testing coverage directory structure
-# 5 subdirectories with 2 files each.
+# 5 subdirectories with 2 indexed chromosomes each.
 @pytest.fixture()
 def mock_coverage_dir():
     with TempDirectory() as dir:
-        for bindir in ['full', 'bin_1', 'bin_25e-2', 'bin_50e-2', 'bin_75e-2']:
+        for bindir in ['full', 'bin_1.00', 'bin_0.25', 'bin_0.50', 'bin_0.75']:
             dirpath = dir.makedir(bindir)
-            Path(os.path.join(dirpath, 'foo.json.gz')).touch()
-            Path(os.path.join(dirpath, 'bar.json.gz')).touch()
+            Path(os.path.join(dirpath, 'chr1.json.gz')).touch()
+            Path(os.path.join(dirpath, 'chr1.json.gz.tbi')).touch()
+            Path(os.path.join(dirpath, 'chr2.json.gz')).touch()
+            Path(os.path.join(dirpath, 'chr2.json.gz.tbi')).touch()
         yield dir
