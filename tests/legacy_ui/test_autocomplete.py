2568778c14ed378065be0a1f7fb4bdf4835350af
Colin Gross <grosscol@gmail.com>
2021-07-02 20:59:58 -0400

    Begin import of UI business logic.

---
+++ tests/legacy_ui/test_autocomplete.py
@@ -0,0 +1,15 @@
+from bravo_api.blueprints.legacy_ui import ui
+
+
+def test_search_genes_names_empty_query():
+    result = ui.search_gene_names('')
+    pass
+
+
+def test_search_variant_ids_empty_query():
+    result = ui.search_variant_ids('')
+    pass
+
+
+def test_search_autocomplete():
+    pass
