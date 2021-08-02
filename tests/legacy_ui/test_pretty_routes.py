a8880f5f8ee1e0fd403c4aa0c13eb358fa0c9020
Colin Gross <grosscol@gmail.com>
2021-08-02 19:05:13 -0400

    Complete initial UI flask endpoints import.

--- tests/legacy_ui/test_pretty_routes.py
+++ tests/legacy_ui/test_pretty_routes.py
@@ -123,8 +123,6 @@ def test_coverage_alias(mocker):
 
 
 def test_coverage_redirect(mocker):
-    mocker.patch('bravo_api.api.get_coverage', side_effect=mock_json_response)
-
     # path args
     chrom = '11'
     start = 500000
@@ -228,4 +226,205 @@ def test_region_histogram_get_alias(mocker):
     with app.test_client() as client:
         client.get(f'/variants/region/snv/{chrom}-{start}-{stop}/histogram')
 
+    mock.assert_called_with(expected_args)
+
+
+def test_region_summary_post_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_region_snv_summary', side_effect=mock_json_response)
+
+    # path args
+    chrom = '11'
+    start = 500000
+    stop = 5001000
+    # post body content
+    body = {'filters': [{'field': 'filter', 'type': '=', 'value': 'PASS'},
+                        {'field': 'annotation.gene.consequence', 'type': '=',
+                         'value': 'frameshift_variant'}],
+            'introns': True}
+
+    expected_args = {'chrom': chrom, 'start': start, 'stop': stop,
+                     'annotation.gene.consequence': 'eq:frameshift_variant', 'filter': 'eq:PASS'}
+
+    with app.test_client() as client:
+        client.post(f'/variants/region/snv/{chrom}-{start}-{stop}/summary', json=body)
+
+    mock.assert_called_with(expected_args)
+
+
+def test_region_summary_get_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_region_snv_summary', side_effect=mock_json_response)
+
+    # path args
+    chrom = '11'
+    start = 500000
+    stop = 5001000
+
+    expected_args = {'chrom': chrom, 'start': start, 'stop': stop}
+
+    with app.test_client() as client:
+        client.get(f'/variants/region/snv/{chrom}-{start}-{stop}/summary')
+
+    mock.assert_called_with(expected_args)
+
+
+def test_gene_summary_post_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_gene_snv_summary', side_effect=mock_json_response)
+
+    # path args
+    name = 'ENSG00000244734'
+
+    body = {'filters': [{'field': 'filter', 'type': '=', 'value': 'PASS'},
+                        {'field': 'annotation.gene.consequence', 'type': '=',
+                         'value': 'synonymous_variant'},
+                        {'field': 'annotation.gene.lof', 'type': '=', 'value': 'HC'}],
+            'introns': True}
+
+    expected_args = {'name': name,
+                     'annotation.gene.consequence': 'eq:synonymous_variant',
+                     'annotation.gene.lof': 'eq:HC',
+                     'filter': 'eq:PASS',
+                     'introns': True}
+
+    with app.test_client() as client:
+        client.post(f'/variants/gene/snv/{name}/summary', json=body)
+
+    mock.assert_called_with(expected_args)
+
+
+def test_gene_summary_get_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_gene_snv_summary', side_effect=mock_json_response)
+
+    # path args
+    name = 'ENSG00000244734'
+
+    expected_args = {'name': name}
+
+    with app.test_client() as client:
+        client.get(f'/variants/gene/snv/{name}/summary')
+
+    mock.assert_called_with(expected_args)
+
+
+def test_gene_histogram_post_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_gene_snv_histogram', side_effect=mock_json_response)
+
+    # path args
+    name = 'ENSG00000244734'
+    # post body content
+    body = {"filters": [{"field": "filter", "type": "=", "value": "PASS"}],
+            "introns": True, "windows": 564}
+
+    expected_args = {'name': name, 'filter': 'eq:PASS', 'windows': 564, 'introns': True}
+
+    with app.test_client() as client:
+        client.post(f'/variants/gene/snv/{name}/histogram', json=body)
+
+    mock.assert_called_with(expected_args)
+
+
+def test_gene_histogram_get_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_gene_snv_histogram', side_effect=mock_json_response)
+
+    # path args
+    name = 'ENSG00000244734'
+
+    expected_args = {'name': name}
+
+    with app.test_client() as client:
+        client.get(f'/variants/gene/snv/{name}/histogram')
+
+    mock.assert_called_with(expected_args)
+
+
+def test_variants_redirect(mocker):
+    # path args
+    chrom = '11'
+    start = 500000
+    stop = 5001000
+    # body args
+    size = 100
+    next = 'https://www.example.com/foo'
+
+    with app.test_client() as client:
+        snv_resp = client.post(f'/variants/region/snv/{chrom}-{start}-{stop}',
+                               json={'size': size, 'next': next},
+                               follow_redirects=False)
+        sv_resp = client.post(f'/variants/region/snv/{chrom}-{start}-{stop}',
+                              json={'size': size, 'next': next},
+                              follow_redirects=False)
+    assert snv_resp.status_code == 303
+    assert snv_resp.location == next
+    assert sv_resp.status_code == 303
+    assert sv_resp.location == next
+
+
+def test_variants_post_alias(mocker):
+    mock_sv = mocker.patch('bravo_api.api.get_region', side_effect=mock_json_response)
+    mock_snv = mocker.patch('bravo_api.api.get_region_snv', side_effect=mock_json_response)
+
+    # path args
+    chrom = '11'
+    start = 500000
+    stop = 5001000
+    # post body content
+    body = {'filters': [{'field': 'filter', 'type': '=', 'value': 'PASS'},
+                        {'field': 'annotation.gene.consequence', 'type': '=',
+                         'value': 'frameshift_variant'}],
+            'introns': True}
+
+    expected_args = {'chrom': chrom, 'start': start, 'stop': stop,
+                     'annotation.gene.consequence': 'eq:frameshift_variant', 'filter': 'eq:PASS'}
+
+    with app.test_client() as client:
+        client.post(f'/variants/region/snv/{chrom}-{start}-{stop}', json=body)
+        client.post(f'/variants/region/sv/{chrom}-{start}-{stop}', json=body)
+    mock_sv.assert_called_with(expected_args)
+    mock_snv.assert_called_with(expected_args)
+
+
+def test_variants_get_alias(mocker):
+    mock_sv = mocker.patch('bravo_api.api.get_region', side_effect=mock_json_response)
+    mock_snv = mocker.patch('bravo_api.api.get_region_snv', side_effect=mock_json_response)
+
+    # path args
+    chrom = '11'
+    start = 500000
+    stop = 5001000
+
+    expected_args = {'chrom': chrom, 'start': start, 'stop': stop}
+
+    with app.test_client() as client:
+        client.get(f'/variants/region/snv/{chrom}-{start}-{stop}')
+        client.get(f'/variants/region/sv/{chrom}-{start}-{stop}')
+    mock_sv.assert_called_with(expected_args)
+    mock_snv.assert_called_with(expected_args)
+
+
+def test_gene_variants_redirect(mocker):
+    # path args
+    name = 'ENSG00000244734'
+    size = 10000
+    next = 'https://www.example.com/foo?bar=baz&duq=qux'
+
+    with app.test_client() as client:
+        resp = client.post(f'/variants/gene/snv/{name}',
+                           json={'size': size, 'next': next}, follow_redirects=False)
+    assert resp.status_code == 303
+    assert resp.location == next
+
+
+def test_gene_variants_post_alias(mocker):
+    mock = mocker.patch('bravo_api.api.get_gene_snv', side_effect=mock_json_response)
+
+    # path args
+    name = 'ENSG00000244734'
+    # post body content
+    body = {"filters": [{"field": "filter", "type": "=", "value": "PASS"}],
+            "introns": True,
+            "size": 1000}
+
+    expected_args = {'name': name, 'filter': 'eq:PASS', 'limit': 1000, 'introns': True}
+
+    with app.test_client() as client:
+        client.post(f'/variants/gene/snv/{name}', json=body)
     mock.assert_called_with(expected_args)
