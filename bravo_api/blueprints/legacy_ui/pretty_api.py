22d88c913271d4743f37d1a2dd3130da10020d81
Colin Gross <grosscol@gmail.com>
2022-01-27 15:25:18 -0500

    Illustrate and test ui_filters to user_filters conversion.   Improve code clarity.   Simplify code path.

--- bravo_api/blueprints/legacy_ui/pretty_api.py
+++ bravo_api/blueprints/legacy_ui/pretty_api.py
@@ -1,4 +1,71 @@
+"""@package Bravo Pretty API
+Two main responsibilities are:
+    - Converting user facing args to underlying model calls.
+    - Aggregate results to data structure expected by web serving layer.
+"""
 from bravo_api.models import variants, coverage
+
+
+FILTER_TYPE_MAPPING = {
+    '=':  '$eq',
+    '!=': '$ne',
+    '<':  '$lt',
+    '>':  '$gt',
+    '<=': '$lte',
+    '>=': '$gte'
+}
+
+
+def convert_to_op_val(tipe, value):
+    """
+    @return list wrapping op: val dict to facilitate appending to list of op: val dicts.
+    """
+    op = FILTER_TYPE_MAPPING.get(tipe)
+    val = [value]
+    return([{op: val}])
+
+
+def nest_dot_fields(op_filters):
+    """
+    Mutate op-val filters to apply nesting to field names that had dots in them.
+    """
+    nestable_keys = [key for key in op_filters if '.' in key]
+    for key_to_nest in nestable_keys:
+        # Remove key and store the op_vals from it
+        op_vals = op_filters.pop(key_to_nest)
+        levels = key_to_nest.split('.')
+        # Begin with innermost level where op_vals will be stored
+        nest = {levels.pop(): op_vals}
+        # Remaining levels become more dict wrapping. Reverse to do inner to outer
+        for level in reversed(levels):
+            nest = {level: nest}
+        # Add nest back to the op_filters.
+        op_filters.update(nest)
+    return(op_filters)
+
+
+def munge_ui_filters(filters):
+    """
+    Take filters formatted by UI and produce user_filters suitable to pass to model functions.
+
+    @param filters a list of dicts with keys field, type, and value
+    @return dict keyed by the unique input field value with type and value aggregated to a list.
+       {field1: [{op: [val]}, {op: [val]}, ...],
+        field2: [{op: [val]}, {op: [val]}, ...],
+        nested3:{ some3:{ field3: [{op: [val]}, {op: [val]}, ...]}}
+       }
+    """
+    op_filters = {}
+    for filt in filters:
+        # Each field value in filters should be a key in the result
+        op_val = op_filters.setdefault(filt['field'], [])
+        # The corresponding type and value are appended the {op: val} list
+        op_val += convert_to_op_val(filt['type'], filt['value'])
+
+    # Mutate result to when nested fields are present.
+    user_filters = nest_dot_fields(op_filters)
+
+    return(user_filters)
 
 
 def get_genes_by_name(name='', full=1):
@@ -17,22 +84,20 @@ def get_genes_in_region(chrom, start, stop, full=1):
     return(result)
 
 
-# continue_from replaces 'last' of previous versions.
 def get_coverage(chrom, start, stop, limit, continue_from=None):
     cov = coverage.get_coverage(chrom, start, stop, limit, continue_from)
 
-    continue_from = None
-    if not cov['stop_reached']:
+    if cov['stop_reached']:
+        continue_from = None
+    else:
         continue_from = cov["last"]
 
     result = {'data': cov['data'], 'total': cov['total'], 'limit': limit,
               'next': continue_from, 'error': None}
     return result
 
-    # if result['last'] is not None:
-    #     url = request.base_url + '?' + '&'.join(f'{arg}={value}' for arg, value in request.args.items(True) if arg != 'last')
-    #     url += f'&last={result["last"]}'
 
-    # response = make_response(jsonify({ 'data': result['data'], 'total': result['total'], 'limit': args['limit'], 'next': continue_from, 'error': None }), 200)
-    # response.mimetype = 'application/json'
-    # return response
+def get_gene_snv_summary(ensembl_id, filters, introns):
+    munged_filters = munge_ui_filters(filters)
+    data = variants.get_gene_snv_summary(ensembl_id, munged_filters, introns)
+    return data
---
+++ tests/legacy_ui/test_filter_conversion.py
@@ -0,0 +1,78 @@
+import pytest
+from bravo_api.blueprints.legacy_ui import pretty_api
+
+
+# process UI filters to internal api "user filters"
+def test_munging_nesting_filters():
+    filters = [
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'start_retained_variant'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'stop_retained_variant'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'synonymous_variant'}]
+
+    expected_result = {'annotation': {'gene': {'consequence': [
+        {'$eq': ['start_retained_variant']},
+        {'$eq': ['stop_retained_variant']},
+        {'$eq': ['synonymous_variant']}
+    ]}}}
+
+    result = pretty_api.munge_ui_filters(filters)
+    assert(expected_result == result)
+
+
+def test_munging_numeric_filters():
+    filters = [{'field': 'allele_freq', 'type': '>', 'value': 0.001},
+               {'field': 'allele_freq', 'type': '<', 'value': 0.98},
+               {'field': 'cadd_phred', 'type': '>', 'value': 0.2},
+               {'field': 'cadd_phred', 'type': '<', 'value': 66}]
+
+    expected_result = {
+        'allele_freq': [{'$gt': [0.001]}, {'$lt': [0.98]}],
+        'cadd_phred': [{'$gt': [0.2]}, {'$lt': [66.0]}]
+    }
+
+    result = pretty_api.munge_ui_filters(filters)
+    assert(expected_result == result)
+
+
+def test_munging_quality_filters():
+    filters = [{'field': 'filter', 'type': '=', 'value': 'PASS'},
+               {'field': 'filter', 'type': '=', 'value': 'SVM'},
+               {'field': 'filter', 'type': '=', 'value': 'DISC'},
+               {'field': 'filter', 'type': '=', 'value': 'EXHET'}]
+
+    expected_result = {
+        'filter': [{'$eq': ['PASS']}, {'$eq': ['SVM']}, {'$eq': ['DISC']}, {'$eq': ['EXHET']}]
+    }
+
+    result = pretty_api.munge_ui_filters(filters)
+    assert(expected_result == result)
+
+
+def test_munging_mess_of_filters():
+    filters = [
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'synonymous_variant'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'frameshift_variant'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'stop_lost'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'transcript_ablation'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'protein_altering_variant'},
+        {'field': 'annotation.gene.consequence', 'type': '=', 'value': 'regulatory_region_variant'},
+        {'field': 'filter', 'type': '=', 'value': 'PASS'},
+        {'field': 'allele_freq', 'type': '>', 'value': 0.001},
+        {'field': 'allele_freq', 'type': '<', 'value': 0.99}]
+
+    expected_result = {
+        'filter': [{'$eq': ['PASS']}],
+        'allele_freq': [{'$gt': [0.001]}, {'$lt': [0.99]}],
+        'annotation': {'gene': {'consequence': [
+            {'$eq': ['synonymous_variant']},
+            {'$eq': ['frameshift_variant']},
+            {'$eq': ['stop_lost']},
+            {'$eq': ['transcript_ablation']},
+            {'$eq': ['protein_altering_variant']},
+            {'$eq': ['regulatory_region_variant']}
+            ]
+        }}
+    }
+
+    result = pretty_api.munge_ui_filters(filters)
+    assert(expected_result == result)
