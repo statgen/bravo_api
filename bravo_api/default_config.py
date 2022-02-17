4e475e645e06911447535245efbefc55f93e225c
Colin Gross <grosscol@gmail.com>
2022-02-17 10:36:04 -0500

    fixup

--- bravo_api/default_config.py
+++ bravo_api/default_config.py
@@ -2,4 +2,4 @@
 LOGIN_DISABLED = True
 GOOGLE_OAUTH_SECRETS_FILE = ""
 SESSION_SECRET = b'deadbeef0123456789'
-UI_ORIGINS = ['http://localhost:8080', 'http://127.0.0.1:8080']
+CORS_ORIGINS = ['http://localhost:8080', 'http://127.0.0.1:8080']
