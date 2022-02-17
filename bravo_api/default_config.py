0e2d27c803a08d248dc4e0590ec86e31ac96b2f7
Colin Gross <grosscol@gmail.com>
2022-02-16 19:38:48 -0500

    Re-implement default config   Provide values for optional config settings.

---
+++ bravo_api/default_config.py
@@ -0,0 +1,5 @@
+# Default Optional configuration
+LOGIN_DISABLED = True
+GOOGLE_OAUTH_SECRETS_FILE = ""
+SESSION_SECRET = b'deadbeef0123456789'
+UI_ORIGINS = ['http://localhost:8080', 'http://127.0.0.1:8080']
