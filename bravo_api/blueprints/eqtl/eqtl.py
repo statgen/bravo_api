4e8c04461bbfc0838e85b4c0827ef1deb68e8eb6
Colin Gross <grosscol@gmail.com>
2025-02-17 12:14:03 -0500

    Consolidate Cond eQTL functions as prep for removal

--- bravo_api/blueprints/eqtl/eqtl.py
+++ bravo_api/blueprints/eqtl/eqtl.py
@@ -1,7 +1,7 @@
 from flask import current_app, Blueprint, jsonify, make_response, Response
 from webargs import fields
 from webargs.flaskparser import FlaskParser
-from marshmallow import RAISE
+from marshmallow import RAISE, validate
 import logging
 
 
@@ -35,6 +35,15 @@ eqtl_cpra_argmap = {
                        error_messages={'validator_failed': 'Value must be a non-empty string.'})
 }
 
+region_argmap = {
+    'chrom': fields.Str(required=True, validate=validate.Length(min=1),
+                        error_messages={'invalid_string': 'String must not be empty.'}),
+    'start': fields.Int(required=True, validate=validate.Range(min=1),
+                        error_messages={'invalid_value': 'Value must be greater than 0.'}),
+    'stop': fields.Int(required=True, validate=validate.Range(min=1),
+                       error_messages={'invalid_value': 'Value must be greater than 0.'})
+}
+
 
 @bp.route('/eqtl/susie', methods=['GET'])
 @parser.use_args(eqtl_argmap, location='query')
@@ -46,27 +55,10 @@ def get_susie(args: dict) -> Response:
     return make_response(jsonify(result))
 
 
-@bp.route('/eqtl/cond', methods=['GET'])
-@parser.use_args(eqtl_argmap, location='query')
-def get_cond(args: dict) -> Response:
-    if args['gene'].startswith("ENSG"):
-        result = cond_ensembl(args['gene'])
-    else:
-        result = cond(args['gene'])
-    return make_response(jsonify(result))
-
-
 @bp.route('/eqtl/susie_count', methods=['GET'])
 @parser.use_args(ensg_argmap, location='query')
 def get_susie_count(args: dict) -> Response:
     result = susie_count(args['ensembl'])
-    return make_response(jsonify(result))
-
-
-@bp.route('/eqtl/cond_count', methods=['GET'])
-@parser.use_args(ensg_argmap, location='query')
-def get_cond_count(args: dict) -> Response:
-    result = cond_count(args['ensembl'])
     return make_response(jsonify(result))
 
 
@@ -77,10 +69,10 @@ def get_susie_by_id(args: dict) -> Response:
     return make_response(jsonify(result))
 
 
-@bp.route('/eqtl/cond_by_id', methods=['GET'])
-@parser.use_args(eqtl_cpra_argmap, location='query')
-def get_cond_by_id(args: dict) -> Response:
-    result = cond_by_id(args['cpra'])
+@bp.route('/eqtl/region', methods=['GET'])
+@parser.use_args(region_argmap, location='query')
+def eqtl_region(args: dict) -> Response:
+    result = eqtl_in_region(args['chrom'], args['start'], args['stop'])
     return make_response(jsonify(result))
 
 
@@ -122,18 +114,67 @@ def susie_by_id(cpra: str) -> dict:
         return answer
 
 
-def cond_by_id(cpra: str) -> dict:
-    """ Lookup single Susie eqtl by positional id.
-    @param cpra.  Positional id in the form of chrom_pos_ref_alt
-    """
-    pipeline = [{'$match': {'variant_id': cpra}}, {'$project': {'_id': False}}]
+def susie_ensembl(ensembl_id: str) -> list:
+    pipeline = [{'$match': {'phenotype_id': ensembl_id}}, {'$project': {'_id': False}}]
+    cursor = current_app.mmongo.db.eqtl_susie.aggregate(pipeline)
+    answer = [item for item in cursor]
+    return answer
+
+
+def susie_count(ensembl_id: str) -> int:
+    mongo_filter = {'phenotype_id': ensembl_id}
+    result = current_app.mmongo.db.eqtl_susie.count_documents(mongo_filter)
+    return result
+
+
+def eqtl_in_region(chrom: str, start: int, stop: int) -> list:
+    pos_pipeline = [{'$match': {'chrom': chrom, 'pos': {'$gte': start, '$lte': stop}}},
+                    {'$project': {'_id': False}}]
+    cursor = current_app.mmongo.db.eqtl_susie.aggregate(pos_pipeline)
+    answer = [item for item in cursor]
+    return answer
+
+
+##########################################
+# Conditional eQTL data will not be used #
+#  Will be removed                       #
+##########################################
+
+@bp.route('/eqtl/cond', methods=['GET'])
+@parser.use_args(eqtl_argmap, location='query')
+def get_cond(args: dict) -> Response:
+    if args['gene'].startswith("ENSG"):
+        result = cond_ensembl(args['gene'])
+    else:
+        result = cond(args['gene'])
+    return make_response(jsonify(result))
+
+
+@bp.route('/eqtl/cond_count', methods=['GET'])
+@parser.use_args(ensg_argmap, location='query')
+def get_cond_count(args: dict) -> Response:
+    result = cond_count(args['ensembl'])
+    return make_response(jsonify(result))
+
+
+@bp.route('/eqtl/cond_by_id', methods=['GET'])
+@parser.use_args(eqtl_cpra_argmap, location='query')
+def get_cond_by_id(args: dict) -> Response:
+    result = cond_by_id(args['cpra'])
+    return make_response(jsonify(result))
+
+
+def cond_ensembl(ensembl_id: str) -> list:
+    pipeline = [{'$match': {'phenotype_id': ensembl_id}}, {'$project': {'_id': False}}]
     cursor = current_app.mmongo.db.eqtl_cond.aggregate(pipeline)
     answer = [item for item in cursor]
+    return answer
 
-    if answer is None:
-        return []
-    else:
-        return answer
+
+def cond_count(ensembl_id: str) -> int:
+    mongo_filter = {'phenotype_id': ensembl_id}
+    result = current_app.mmongo.db.eqtl_cond.count_documents(mongo_filter)
+    return result
 
 
 def cond(gene_name: str) -> list:
@@ -156,27 +197,15 @@ def cond(gene_name: str) -> list:
         return answer['eqtls']
 
 
-def susie_ensembl(ensembl_id: str) -> list:
-    pipeline = [{'$match': {'phenotype_id': ensembl_id}}, {'$project': {'_id': False}}]
-    cursor = current_app.mmongo.db.eqtl_susie.aggregate(pipeline)
-    answer = [item for item in cursor]
-    return answer
-
-
-def cond_ensembl(ensembl_id: str) -> list:
-    pipeline = [{'$match': {'phenotype_id': ensembl_id}}, {'$project': {'_id': False}}]
+def cond_by_id(cpra: str) -> dict:
+    """ Lookup single Susie eqtl by positional id.
+    @param cpra.  Positional id in the form of chrom_pos_ref_alt
+    """
+    pipeline = [{'$match': {'variant_id': cpra}}, {'$project': {'_id': False}}]
     cursor = current_app.mmongo.db.eqtl_cond.aggregate(pipeline)
     answer = [item for item in cursor]
-    return answer
 
-
-def susie_count(ensembl_id: str) -> int:
-    mongo_filter = {'phenotype_id': ensembl_id}
-    result = current_app.mmongo.db.eqtl_susie.count_documents(mongo_filter)
-    return result
-
-
-def cond_count(ensembl_id: str) -> int:
-    mongo_filter = {'phenotype_id': ensembl_id}
-    result = current_app.mmongo.db.eqtl_cond.count_documents(mongo_filter)
-    return result
+    if answer is None:
+        return []
+    else:
+        return answer
