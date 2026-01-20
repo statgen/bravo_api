e9b5eddba95ab86021b991456efac36e4f51fe18
Colin Gross <grosscol@gmail.com>
2026-01-20 12:28:29 -0500

    Document S3 and domain restriction config options.

--- config_example.py
+++ config_example.py
@@ -4,11 +4,16 @@ import os.path
 MONGO_URI = 'mongodb://localhost:27017/bravo-demo'
 BRAVO_API_PAGE_LIMIT = 10000
 
+# runtime directory in the unpacked vignette or from completed data prep run.
 BASE_DIR = os.path.join(os.sep, 'var', 'local', 'bravo', 'data', 'runtime')
+
+# File system path or S3 path to coverage and crams directories
 COVERAGE_DIR = os.path.join(BASE_DIR, 'coverage')
 SEQUENCES_DIR = os.path.join(BASE_DIR, 'crams')
-SEQUENCES_CACHE_DIR = os.path.join(BASE_DIR, 'cache')
+
 REFERENCE_SEQUENCE = os.path.join(BASE_DIR, 'reference', 'chr11_hs38DH.fa')
+SEQUENCES_CACHE_DIR = os.path.join(BASE_DIR, 'cache')
+
 
 # Optional configuration
 LOGIN_DISABLED = True
@@ -19,3 +24,9 @@ CORS_ORIGINS = ['http://localhost:8080']
 GOOGLE_CLIENT_ID = "your google oauth client id"
 GOOGLE_CLIENT_SECRET = "your google oauth client secret"
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+# File system path to public directories or S3 bucket path
+PUBVCFS_DIR = os.path.join(os.sep, 'var', 'local', 'bravo', 'data', 'runtime', 'pubvcfs')
+
+# When supplied, will restruct authorized users to a single domain
+USER_DOMAIN_PERMITTED = ""
